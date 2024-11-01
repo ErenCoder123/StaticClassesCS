08b1a48e175d61c203382fea2a76b9f52f999a4c
Miguel Cardoso <migueltranslatorbr@gmail.com>
2024-10-31 21:22:51 -0300

    commitando

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(ConversorTemperatura.CelsiusParaFahrenheit(16));
+Console.WriteLine(ConversorTemperatura.FahrenheitParaCelsius(16));
+
+Configuracoes.NomeAplicacao = "teste";
+Configuracoes.Versao = 1;
+
+Console.WriteLine(Configuracoes.NomeAplicacao);
+Console.WriteLine(Configuracoes.Versao);
